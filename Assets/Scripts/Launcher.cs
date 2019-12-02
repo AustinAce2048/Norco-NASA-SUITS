3b06aa5d064ddb44ecba985cd8f8bc21a1030ebd
Austin <austinw2048@gmail.com>
2019-12-02 14:18:34 -0800

    After 17 attempts I'm sure the LeapMotion cannot be integrated into OQ

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -7,13 +7,25 @@ using Photon.Realtime;
 public class Launcher : MonoBehaviourPunCallbacks {
 
     public string gameVersion = "1";
-    public GameObject leapRig;
+    public Transform leftHand;
+    public Transform rightHand;
+    public GameObject leftSphere;
+    public GameObject rightSphere;
 
     void Awake () {
         PhotonNetwork.AutomaticallySyncScene = true;
     }
     void Start () {
         Connect ();
+    }
+
+    void Update () {
+        if (GameObject.Find ("Leap Rig(Clone)") != null) {
+            leftHand = GameObject.Find ("Leap Rig(Clone)").transform.GetChild (1).transform.GetChild (0);
+            rightHand = GameObject.Find ("Leap Rig(Clone)").transform.GetChild (1).transform.GetChild (1);
+            leftSphere.transform.position = new Vector3 (leftHand.transform.position.x, leftHand.transform.position.y, leftHand.transform.position.z);
+            rightSphere.transform.position = new Vector3 (rightHand.transform.position.x, rightHand.transform.position.y, rightHand.transform.position.z);
+        }
     }
 
     public void Connect () {
@@ -37,7 +49,18 @@ public class Launcher : MonoBehaviourPunCallbacks {
 
     public override void OnJoinedRoom () {
         Debug.Log ("Client joined room");
-        PhotonNetwork.Instantiate ("Leap Rig", new Vector3 (0f, 1.6f, 0f), Quaternion.identity, 0);
+        if (PhotonNetwork.IsMasterClient) {
+            StartCoroutine (leapSpawn ());
+        }
+    }
+
+    IEnumerator leapSpawn () {
+        yield return new WaitForSeconds (15f);
+        PhotonNetwork.InstantiateSceneObject ("Leap Rig", new Vector3 (0f, 1.6f, 0f), Quaternion.identity, 0);
+    }
+
+    public void OnPhotonPlayerConnected () {
+        Debug.Log ("Connected");
     }
 
     public override void OnDisconnected (DisconnectCause cause) {
