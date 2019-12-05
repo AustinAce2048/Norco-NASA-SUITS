33f0fe4ab697afd5cc364156caf1db0563b95801
Austin <austinw2048@gmail.com>
2019-12-05 14:19:37 -0800

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour {
+
+    public Transform target;
+
+    void Update () {
+        if (target != null) {
+            transform.LookAt (target);
+       }
+    }
+
+}
---
+++ Assets/Scripts/RoverRepairSteps.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoverRepairSteps : MonoBehaviour {
+
+    public string currentStep = "";
+
+    void Start () {
+        currentStep = "Get to rover";
+    }
+
+    void Update () {
+        if (currentStep == "Get to rover") {
+            
+        }
+    }
+
+}
