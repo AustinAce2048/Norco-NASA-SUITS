8edf04a7c25dbdbc48c207026aed47978013d2bc
Austin <austinw2048@gmail.com>
2020-01-11 08:52:30 -0800

    Updated player prefab to disable controlling 4 hands

---
+++ Assets/Scripts/PUNPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PUNPlayer : MonoBehaviour {
+
+    void Start () {
+        if (PhotonNetwork.IsMasterClient == true) {
+            //Enable components
+            transform.GetChild (0).GetComponent<OVRCameraRig> ().enabled = true;
+            transform.GetChild (0).GetComponent<OVRManager> ().enabled = true;
+        }
+    }
+
+}
