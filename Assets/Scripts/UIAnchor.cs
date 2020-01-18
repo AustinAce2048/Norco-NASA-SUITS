622a34a5c55c332c705e8e3125cece2aaafe26fd
Austin <austinw2048@gmail.com>
2020-01-18 13:12:51 -0800

    Getting hand root object

---
+++ Assets/Scripts/UIAnchor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAnchor : MonoBehaviour {
+
+    public GameObject worldUIRoot;
+    public GameObject anchorPoint;
+
+    void Update () {
+        worldUIRoot.transform.position = anchorPoint.transform.position;
+    }
+
+}
