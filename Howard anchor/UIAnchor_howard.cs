dcc83278c5f35faea966ccaa6b2b9d892927ce4c
Chaoward <57738845+Chaoward@users.noreply.github.com>
2020-01-28 22:14:36 -0800

    Added my little edits to the UI Anchor script
    Please test to see if it works of not

---
+++ Howard anchor/UIAnchor_howard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAnchor_howard : MonoBehaviour
+{
+
+    public GameObject worldUIRoot;
+
+    void Update () {
+        worldUIRoot.transform.position = GetComponent<OVRSkeleton> ().Bones[3].Transform.position;
+    }
+
+}
