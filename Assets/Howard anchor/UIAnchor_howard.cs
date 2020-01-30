499f39e06499366f1030d0618506c8fa0410a35e
Austin <austinw2048@gmail.com>
2020-01-29 17:37:40 -0800

    Todays meeting 1/29

--- Assets/Howard anchor/UIAnchor_howard.cs
+++ Assets/Howard anchor/UIAnchor_howard.cs
@@ -6,9 +6,20 @@ public class UIAnchor_howard : MonoBehaviour
 {
 
     public GameObject worldUIRoot;
+    private Transform thumbBonePosition;
+    private Transform palm;
+
+    void Start(){
+        thumbBonePosition = GetComponent<OVRSkeleton> ().Bones[3].Transform;
+        palm = GetComponent<OVRSkeleton> ().Bones[0].Transform;
+    }
 
     void Update () {
-        worldUIRoot.transform.position = GetComponent<OVRSkeleton> ().Bones[3].Transform.position;
+        if (Physics.Raycast(new Vector3(palm.position.x, palm.position.y, palm.position.z),new Vector3(0, 0, 1), 30f, 0, ddefault)){
+            worldUIRoot.SetActive(true);
+            worldUIRoot.transform.position = new Vector3 (thumbBonePosition.position.x, thumbBonePosition.position.y + 0.05f, thumbBonePosition.position.z);
+        }
+        else { worldUIRoot.SetActive(false); }
     }
 
 }
