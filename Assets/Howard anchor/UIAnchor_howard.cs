e93c8933ed4377288d520499d4944c4da1ed7e4e
Austin <austinw2048@gmail.com>
2020-02-05 19:18:17 -0800

    Changes you guys did

--- Assets/Howard anchor/UIAnchor_howard.cs
+++ Assets/Howard anchor/UIAnchor_howard.cs
@@ -8,15 +8,20 @@ public class UIAnchor_howard : MonoBehaviour {
     public GameObject cameraRoot;
     private Transform pinkyBonePosition;
     //private Transform wristBonePosition;
-    //private Transform palmBonePosition;
+    private Transform palmBonePosition;
+
+    //Ray ray;
 
     void Start () {
         pinkyBonePosition = GetComponent<OVRSkeleton> ().Bones[16].Transform;
         //wristBonePosition = GetComponent<OVRSkeleton> ().Bones[1].Transform;
-        //palmBonePosition = GetComponent<OVRSkeleton> ().Bones[0].Transform;
+        palmBonePosition = GetComponent<OVRSkeleton> ().Bones[0].Transform;
+
     }
 
     void Update () {
+        //worldUIRoot.transform.parent = palmBonePosition; //ignore
+
         worldUIRoot.transform.position = new Vector3 (pinkyBonePosition.position.x - 0.1f, pinkyBonePosition.position.y, pinkyBonePosition.position.z);
         //worldUIRoot.transform.rotation = Quaternion.Euler (palmBonePosition.rotation.x, palmBonePosition.rotation.y, palmBonePosition.rotation.z);
         //Rotation seems to be absolute
@@ -25,10 +30,13 @@ public class UIAnchor_howard : MonoBehaviour {
         Vector3 targetDir = new Vector3 (0f, 10000f, 0f) - transform.position;
         float angle = Vector3.Angle (targetDir, transform.up);
         if (angle <= 30f) {
-            worldUIRoot.SetActive (true);
-        } else {
             worldUIRoot.SetActive (false);
+        } else {
+            worldUIRoot.SetActive (true);
         }
+
+        //ray = new Ray(transform.position, Vector3.down);
+        //Debug.DrawLine(palmBonePosition.position, palmBonePosition.position + Vector3.down,Color.red);
     }
 
 }
