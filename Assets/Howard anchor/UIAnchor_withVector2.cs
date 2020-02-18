4e4a3e01d0eefb46297875d9697fcd87b2ce4896
Chaoward <57738845+Chaoward@users.noreply.github.com>
2020-02-18 01:49:20 -0800

    script with Vector2 method
    Test this script for UI being active when palm is up.

---
+++ Assets/Howard anchor/UIAnchor_withVector2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NOTE: please apply this script to right hand prefab to test
+public class UIAnchor_withVector2 : MonoBehaviour
+{
+    public GameObject worldUIRoot;
+    private Transform pinkyBonePosition;
+    private Transform palmBonePosition;
+
+    void Start()
+    {
+        pinkyBonePosition = GetComponent<OVRSkeleton>().Bones[16].Transform;
+        //wristBonePosition = GetComponent<OVRSkeleton> ().Bones[1].Transform;
+        //palmBonePosition = GetComponent<OVRSkeleton> ().Bones[0].Transform;
+        palmBonePosition = GetComponent<OVRSkeleton>().Bones[0].Transform;
+
+    }
+
+    void Update()
+    {
+        worldUIRoot.transform.position = GetComponent<OVRSkeleton>().Bones[3].Transform.position;
+
+        float angleXY = Vector2.Angle(Vector2.left, transform.up);
+        float angleXZ = Vector3.Angle(new Vector3(0, 10000f, 0), transform.up);
+        if (angleXY <= 90f && angleXZ <= 30)
+        {
+            worldUIRoot.SetActive(false);
+        }
+        else
+        {
+            worldUIRoot.SetActive(true);
+        }
+    }
+}
