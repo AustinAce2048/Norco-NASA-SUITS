b6dbd2ec1415c88a6e8a48002beaa117012ed4e7
Howard <howieedu@gmail.com>
2020-02-25 01:39:28 -0800

    reorganized anchor slots and attempted buttons with colliders

--- Assets/Howard anchor/UIAnchor_howard.cs
+++ Assets/Howard anchor/UIAnchor_howard.cs
@@ -3,30 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UIAnchor_howard : MonoBehaviour {
-
+    //UI attachments
     public GameObject worldUIRoot;
+    public GameObject panel1;
+    public GameObject wristSlot;
+    public GameObject palmSlot;
     public GameObject cameraRoot;
+
+    //Bones
     private Transform pinkyBonePosition;
-    //private Transform wristBonePosition;
+    private Transform wristBonePosition;
     private Transform palmBonePosition;
 
-    void Start () {
+
+    void Start() {
         pinkyBonePosition = GetComponent<OVRSkeleton> ().Bones[16].Transform;
-        //wristBonePosition = GetComponent<OVRSkeleton> ().Bones[1].Transform;
+        wristBonePosition = GetComponent<OVRSkeleton> ().Bones[1].Transform;
         palmBonePosition = GetComponent<OVRSkeleton> ().Bones[0].Transform;
 
+        worldUIRoot = GameObject.Find("UI Right Hand");
+
+        //==== making the UI element for palmSlot as a child of palm bone. Comment out and uncomment palmSlot.transform.position for position method =============================
+        palmSlot.transform.parent = palmBonePosition;
+        palmSlot.transform.position = new Vector3(palmBonePosition.position.x, palmBonePosition.position.y, palmBonePosition.position.z);
+        //========================================================================================================================================================================
     }
 
-    void Update () {
-        worldUIRoot.transform.position = new Vector3 (pinkyBonePosition.position.x - 0.1f, pinkyBonePosition.position.y, pinkyBonePosition.position.z);
+    void Update() {
+        if (panel1.transform.parent != null) { panel1.transform.position = new Vector3(pinkyBonePosition.position.x - 0.1f, pinkyBonePosition.position.y, pinkyBonePosition.position.z); }
+        wristSlot.transform.position = new Vector3(wristBonePosition.position.x, wristBonePosition.position.y + 5000f, wristBonePosition.position.z - 1000f);
+        //uncomment for position method use
+        //palmSlot.transform.position = new Vector3(palmBonePosition.position.x, palmBonePosition.position.y, palmBonePosition.position.z);
 
-        Vector3 targetDir = new Vector3 (cameraRoot.transform.position.x, cameraRoot.transform.position.y + 10000f, cameraRoot.transform.position.z) - cameraRoot.transform.position;
-        float angle = Vector3.Angle (targetDir, -palmBonePosition.up);
+        Vector3 targetDir = new Vector3(cameraRoot.transform.position.x, cameraRoot.transform.position.y + 10000f, cameraRoot.transform.position.z) - cameraRoot.transform.position;
+        float angle = Vector3.Angle(targetDir, -palmBonePosition.up);
         if (angle <= 30f) {
-            worldUIRoot.SetActive (true);
+            worldUIRoot.SetActive(true);
         } else {
-            worldUIRoot.SetActive (false);
+            worldUIRoot.SetActive(false);
         }
+
     }
 
 }
---
+++ Assets/Howard anchor/panel1_button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panel1_button : MonoBehaviour
+{
+    public bool isAnchored = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isAnchored == true)
+        {
+            isAnchored = false;
+            this.transform.parent = null;
+            this.transform.position += new Vector3(0, 2.5f, 0);
+        }
+        else
+        {
+            isAnchored = true;
+            this.transform.parent = GameObject.Find("UI Right Hand").transform;
+        }
+    }
+    
+}
